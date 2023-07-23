34bf05578b3ad742984e60b28a79de721a6d670c
eliotthergat <eliotthergat@gmail.com>
2023-07-23 14:35:29 +0200

    Update home.py

--- home.py
+++ home.py
@@ -8,5 +8,8 @@ sidebar()
 
 openai_api_key = st.session_state.get("OPENAI_API_KEY")
 
-st.set_page_config(page_title="Khlinic Brain", page_icon="🧠", layout="wide")
+st.set_page_config(
+    page_title="Accueil - Khlinic",
+    page_icon="✍🏻",
+)
 st.header("🧠 Khlinic Brain")
