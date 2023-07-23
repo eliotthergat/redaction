df7b244a773aac89550b202f83f05d090155d582
eliotthergat <eliotthergat@gmail.com>
2023-07-23 14:25:46 +0200

    Update home.py

--- home.py
+++ home.py
@@ -6,19 +6,4 @@ st.set_page_config(
     page_title="Accueil - Khlinic",
     page_icon="✍🏻",
 )
-with st.sidebar:
-    st.image('assets/logo.svg')
-    st.markdown(
-            "## Comment fonctionne l'outil ?\n"
-            "1. Entrez une clé OpenAI\n"
-            "2. Choisissez les liens à explorer\n"
-            "3. Entrez le plan\n"
-            "4. Entrez les mots-clés\n"
-        )
-    api_key_input = st.text_input(
-        "OpenAI API Key",
-        type="password",
-        placeholder="Paste your OpenAI API key here",
-        help="Needed to use the OpenAI API"
-    )
-    st.markdown("---")
+from components.sidebar import sidebar
