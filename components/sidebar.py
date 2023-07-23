97725dbf289b8d804fa3891cbbda4dc318e28fe5
eliotthergat <eliotthergat@gmail.com>
2023-07-23 14:43:04 +0200

    Update sidebar.py

--- components/sidebar.py
+++ components/sidebar.py
@@ -26,7 +26,14 @@ def sidebar():
         )
         st.session_state["OPENAI_API_KEY"] = api_key_input
         st.markdown("---")
+        st.markdown("# Paramètres")
+        PARAMS["max_tokens"] = st.slider("Longueur maximale (`max_tokens`):", min_value=1, max_value=8000, value=2048, step=25)
+        PARAMS["temperature"] = st.slider("Température (`randomness`)", min_value=0.0, max_value=2.0, value=1, step=0.01)
+        PARAMS["presence_penalty"] = st.slider("Pénalité de présence (`presence_penalty`)", min_value=0.0, max_value=2.0, value=0, step=0.1)
+        PARAMS["frequency_penalty"] = st.slider("Pénalité de fréquence (`frequence_penalty`)", min_value=0.0, max_value=2.0, value=0, step=0.1)
+        st.markdown("---")
         st.markdown("# À propos")
         st.markdown(
             "📖 Tous les crédits vont à Khlinic. "
         )
+        
