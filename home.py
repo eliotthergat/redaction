bddbe0c5012a268f36d1e492065fb4dd63efaa61
eliotthergat <eliotthergat@gmail.com>
2023-07-23 14:26:56 +0200

    Update home.py

--- home.py
+++ home.py
@@ -2,8 +2,13 @@ import os
 import openai
 import streamlit as st
 
+from components.sidebar import sidebar
+
 st.set_page_config(
     page_title="Accueil - Khlinic",
     page_icon="✍🏻",
 )
-from components.sidebar import sidebar
+
+sidebar()
+
+openai_api_key = st.session_state.get("OPENAI_API_KEY")
