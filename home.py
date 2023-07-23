ace2d0a6ebbfc1f0be888db71471a5b2d663dfdd
eliotthergat <eliotthergat@gmail.com>
2023-07-23 15:12:56 +0200

    Update home.py

--- home.py
+++ home.py
@@ -22,10 +22,4 @@ sidebar()
 
 openai_api_key = st.session_state.get("OPENAI_API_KEY")
 
-col1, col2, col3 = st.columns([1, 1,1])
-col1.subheader("Écriture 🖋")
-col1.markdown("Pour écrire un article d'après un plan, des mots-clés et des liens.")
-faq = col1.button("Écrire")
-if faq:
-    switch_page("écriture d'un article 🖋")
 
