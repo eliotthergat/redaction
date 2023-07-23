c5f6d558c76e3daa97f5fad7093047b1b487e728
eliotthergat <eliotthergat@gmail.com>
2023-07-23 14:00:08 +0200

    Update home.py

--- home.py
+++ home.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_extras.switch_page_button import switch_page
+# from streamlit_extras.switch_page_button import switch_page
 from streamlit_extras.app_logo import add_logo
 
 st.set_page_config(
