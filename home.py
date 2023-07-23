170356cc1303c40f1a8b5001ccbb59b4068b04e8
eliotthergat <eliotthergat@gmail.com>
2023-07-23 14:39:45 +0200

    Update home.py

--- home.py
+++ home.py
@@ -14,3 +14,9 @@ st.header("🖊️ Khlinic writer")
 sidebar()
 
 openai_api_key = st.session_state.get("OPENAI_API_KEY")
+
+col1, col2, col3 = st.columns([1, 1,1])
+col1.subheader("Écriture 🖋")
+col1.markdown("Pour écrire un article d'après un plan, des mots-clés et des liens.")
+faq = col1.button("Écrire")
+
