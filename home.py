5e41df6936a6be1834f4eb64b918a474a9c6a5fe
eliotthergat <eliotthergat@gmail.com>
2023-07-23 14:06:18 +0200

    Update home.py

--- home.py
+++ home.py
@@ -5,4 +5,17 @@ st.set_page_config(
     page_icon="✍🏻",
 )
 with st.sidebar:
-    openai_api_key = st.text_input('OpenAI API Key')
+    st.markdown(
+            "## How to use\n"
+            "1. Enter your [OpenAI API key](https://platform.openai.com/account/api-keys) below🔑\n"  # noqa: E501
+            "2. Upload a pdf, docx, or txt file📄\n"
+            "3. Ask a question about the document💬\n"
+        )
+        api_key_input = st.text_input(
+            "OpenAI API Key",
+            type="password",
+            placeholder="Paste your OpenAI API key here (sk-...)",
+            help="Needed to use the OpenAI API",  # noqa: E501
+            value=os.environ.get("OPENAI_API_KEY", None)
+            or st.session_state.get("OPENAI_API_KEY", ""),
+        )
