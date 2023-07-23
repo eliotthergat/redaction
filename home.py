0899e32e54b5bf9718e58c4b613a12531c73fb7c
eliotthergat <eliotthergat@gmail.com>
2023-07-23 13:55:06 +0200

    Update home.py

--- home.py
+++ home.py
@@ -1,1 +1,21 @@
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+from streamlit_extras.app_logo import add_logo
 
+st.set_page_config(
+    page_title="Accueil - Khlinic",
+    page_icon="✍🏻",
+)
+add_logo("assets/logo_black.png", height=50)
+
+if "shared" not in st.session_state:
+   st.session_state["shared"] = True
+st.write("# Les outils de rédaction Khlinic ✍🏻")
+st.markdown(
+    """
+    Ces outils permettent de générer des textes pour Khlinic.
+"""
+)
+st.sidebar.subheader("Paramètres")
+PARAMS = {}
+PARAMS["api_key"] = st.text_area("OpenAI API key")
