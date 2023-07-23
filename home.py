7ba7913e91e2caabc24202a3f158bed42897bafd
eliotthergat <eliotthergat@gmail.com>
2023-07-23 21:30:56 +0200

    Update home.py

--- home.py
+++ home.py
@@ -84,8 +84,8 @@ def parser(link):
     else:
         main = soup.find('body')
     cleaned_html = str(main)
-    st.write(cleaned_html)
     markdown_text = markdownify.markdownify(cleaned_html)
+    st.write(str(markdown_text))
     return markdown_text
 def concurrent_analyzer(text):
     response = openai.ChatCompletion.create(
